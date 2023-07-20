287edce6a0d68a72ffcc5e5ab5c8a6bb6e5123c1
Tejas Shinde <Tejas Shinde@DESKTOP-HTD419Q>
2023-07-20 12:04:49 +0530

    First commit

---
+++ dayone/src/org/tnsif/introduction/UerInputDEmo.java
@@ -0,0 +1,29 @@
+//program to demonstrate on user input
+package org.tnsif.introduction;
+import java.util.Scanner;
+public class UerInputDEmo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+Scanner sc= new Scanner(System.in);
+/*System.out.println("enter a number");
+int num=sc.nextInt();
+System.out.println("the value of num is " + num);
+
+
+System.out.println("enter a number");
+float num1=sc.nextFloat();
+System.out.println("the value of num is " + num1);*/
+
+//char c=sc.next().charAt(0);
+//System.out.println(c);
+
+String str=sc.nextLine();
+//sc.next();
+System.out.println("String is : " +str);
+
+
+	}
+
+}
