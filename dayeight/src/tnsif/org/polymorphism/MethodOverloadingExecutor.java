28595ef0a22823a5f1a4cd2455c480c0e151bb62
Tejas Shinde <Tejas Shinde@DESKTOP-HTD419Q>
2023-08-07 23:12:24 +0530

    commit

--- dayeight/src/tnsif/org/polymorphism/MethodOverloadingExecutor.java
+++ dayeight/src/tnsif/org/polymorphism/MethodOverloadingExecutor.java
@@ -1,4 +1,11 @@
 //program to demonstrate on compile time polymorphism
+/*Function overloading comes under the compile-time polymorphism. During compilation, the function signature is checked.
+ *  So, functions can be overloaded, if the signatures are not the same. The return type of a function has no effect on 
+ *  function overloading, therefore the same function signature with different return type will not be overloaded. 
+
+Example: if there are two functions: int sum() and float sum(), these two will generate a compile-time error as function 
+overloading is not possible here.*/
+
 package tnsif.org.polymorphism;
 import java.util.*;
  class Bollywood
@@ -9,10 +16,10 @@ import java.util.*;
 		System.out.println(str1+" loves "+ str2);
 	}
 	
-	static int display(String str1, String str2,String str3)
+	static void display(String str1, String str2,String str3)
 	{
-		System.out.println("unfortunately " + str2 +" gets married with " +str3);
-		return 0;
+		System.out.println("unfortunately " + str2 +" gets married with "+str3 );
+	//return 0;
 	}
 }
 public class MethodOverloadingExecutor {
