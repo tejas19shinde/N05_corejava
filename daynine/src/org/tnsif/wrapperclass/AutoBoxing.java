78258283de151e23bdc08cc7f360b1ba3d3e8b46
Tejas Shinde <Tejas Shinde@DESKTOP-HTD419Q>
2023-08-10 11:37:32 +0530

    commit

---
+++ daynine/src/org/tnsif/wrapperclass/AutoBoxing.java
@@ -0,0 +1,19 @@
+package org.tnsif.wrapperclass;
+/*
+ * program to deminstrate on autoboxing 
+ * autoboxing is the converstion of primitive type to object type*/
+public class AutoBoxing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char c='a';
+		//converting a primitive data type into object data type
+		Character c1=c;
+		System.out.println(c1);
+		Integer a=12;
+		
+		
+
+	}
+
+}
---
+++ daynine/src/org/tnsif/wrapperclass/Unboxing.java
@@ -0,0 +1,13 @@
+package org.tnsif.wrapperclass;
+/*unboxing : conversion of primitive to object type*/
+public class Unboxing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer t=18;
+		int a=t;
+		System.out.println(a);
+
+	}
+
+}
